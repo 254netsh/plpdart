a2a18cbaa51290a2000d96ecc818e2337743ebdc
sam <samgachuiga@gmail.com>
2024-02-24 06:45:32 +0300

    first commit

---
+++ main.dart
@@ -0,0 +1,17 @@
+/*this is  a code showing use of different data types */
+import 'dart:developer';
+
+
+
+void main() { 
+  //list
+ // List<String> service =  List<String>(4);  //.filled(  4 , , growable: false);
+  String service = List<Str>.filled(4, fill) ;
+  service = "machinery";
+  service = "masonary";
+  service= "plumbing";
+  service= "electrical";
+
+  print(service );
+
+}   
